cc13ae43b98972194fa35fee3b2bc0c107a7544f
Michael Rodriguez <me@mrodriguez.me>
2026-07-07 17:35:15 -0400

    Clean up main.c a bit

--- app/main.c
+++ app/main.c
@@ -39,6 +39,9 @@ static const char *prog_name;
 static SDL_Window *window;
 static SDL_Renderer *renderer;
 static SDL_Texture *texture;
+static bool running;
+static Uint64 fps_last_update_ns;
+static Uint32 frame;
 
 static void log_cb(struct p_ctx *const ctx, const struct p_log_msg *const msg)
 {
@@ -94,6 +97,7 @@ static void on_vblank(struct p_ctx *const ctx)
 {
 	SDL_UpdateTexture(texture, NULL, ctx->gpu.vram,
 			  VRAM_WIDTH * sizeof(*ctx->gpu.vram));
+
 	SDL_RenderClear(renderer);
 	SDL_RenderTexture(renderer, texture, NULL, NULL);
 	SDL_RenderPresent(renderer);
@@ -110,12 +114,13 @@ static bool load_bios_file(const char *const bios_file)
 
 	if (file_size != P_BUS_BIOS_SIZE) {
 		fprintf(stderr,
-			"%s: bios file size is not correct (expected %d bytes, got %zu)\n",
+			"%s: bios file size is not correct (expected %d bytes, "
+			"got %zu)\n",
 			prog_name, P_BUS_BIOS_SIZE, file_size);
 		return false;
 	}
 
-	FILE *handle = fopen(bios_file, "rb");
+	FILE *const handle = fopen(bios_file, "rb");
 
 	if (!handle) {
 		fprintf(stderr, "%s: unable to open bios file %s: %s\n",
@@ -123,11 +128,10 @@ static bool load_bios_file(const char *const bios_file)
 		return false;
 	}
 
-	uint8_t *dst = p_bios_data_get(&m_ctx);
-	const size_t bytes_read =
-		fread(dst, sizeof(uint8_t), file_size, handle);
+	uint8_t *const dst = p_bios_data_get(&m_ctx);
+	const size_t bytes = fread(dst, sizeof(uint8_t), file_size, handle);
 
-	if (bytes_read != file_size) {
+	if (bytes != file_size) {
 		fprintf(stderr,
 			"%s: not all bytes were read from bios file %s: %s\n",
 			prog_name, bios_file, strerror(errno));
@@ -173,6 +177,113 @@ static bool load_exe_file(const char *const exe_file)
 	return true;
 }
 
+static void fps_update(void)
+{
+	char title[64];
+	frame++;
+
+	const Uint64 curr_ticks = SDL_GetTicksNS();
+	const Uint64 fps_diff = curr_ticks - fps_last_update_ns;
+
+	if (fps_diff >= 1000000000) {
+		const double fps = frame * 1000000000.0 / (double)fps_diff;
+
+		SDL_snprintf(title, sizeof(title), "psycho - %.1f FPS", fps);
+		SDL_SetWindowTitle(window, title);
+
+		frame = 0;
+		fps_last_update_ns = curr_ticks;
+	}
+}
+
+static bool gfx_init(void)
+{
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+			     "SDL_Init() failed: %s", SDL_GetError());
+		return false;
+	}
+
+	if (!SDL_CreateWindowAndRenderer("psycho", 1920, 1080,
+					 SDL_WINDOW_RESIZABLE, &window,
+					 &renderer)) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+			     "SDL_CreateWindowAndRenderer() failed: %s",
+			     SDL_GetError());
+		return false;
+	}
+
+	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_XBGR1555,
+				    SDL_TEXTUREACCESS_STREAMING, VRAM_WIDTH,
+				    VRAM_HEIGHT);
+
+	if (!texture) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+			     "SDL_CreateTexture() failed: %s", SDL_GetError());
+		return false;
+	}
+
+	SDL_RenderClear(renderer);
+	SDL_RenderTexture(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
+
+	frame = 0;
+	fps_last_update_ns = SDL_GetTicksNS();
+
+	return true;
+}
+
+static void emu_init(void)
+{
+	struct p_ctx_cfg *const cfg = p_cfg_get(&m_ctx);
+
+	cfg->cpu.illegal_instr = illegal_instr_cb;
+
+	cfg->log.log_cb = log_cb;
+
+	// clang-format off
+
+	cfg->log.mod[P_LOG_CTX]     = P_LOG_TRACE;
+	cfg->log.mod[P_LOG_CPU]     = P_LOG_OFF;
+	cfg->log.mod[P_LOG_BUS]     = P_LOG_TRACE;
+	cfg->log.mod[P_LOG_BIOS]    = P_LOG_TRACE;
+	cfg->log.mod[P_LOG_SCHED]   = P_LOG_TRACE;
+	cfg->log.mod[P_LOG_GPU]     = P_LOG_OFF;
+	cfg->log.mod[P_LOG_INTCTRL] = P_LOG_TRACE;
+
+	// clang-format on
+
+	cfg->bios_trace.stdout_line = on_stdout_line;
+	cfg->bios_trace.deref_ptrs = true;
+
+	//cfg->disasm.tracing = true;
+
+	cfg->on_vblank = on_vblank;
+
+	p_init(&m_ctx);
+}
+
+static void gfx_fini(void)
+{
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+}
+
+static void sdl_process_ev(SDL_Event *const ev)
+{
+	switch (ev->type) {
+	case SDL_EVENT_QUIT:
+		running = false;
+		break;
+
+	default:
+		break;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (argc < 3) {
@@ -185,27 +296,7 @@ int main(int argc, char **argv)
 
 	prog_name = argv[0];
 
-	struct p_ctx_cfg *const cfg = p_cfg_get(&m_ctx);
-
-	cfg->cpu.illegal_instr = illegal_instr_cb;
-
-	cfg->log.log_cb = log_cb;
-	cfg->log.mod[P_LOG_CTX] = P_LOG_TRACE;
-	cfg->log.mod[P_LOG_CPU] = P_LOG_OFF;
-	cfg->log.mod[P_LOG_BUS] = P_LOG_TRACE;
-	cfg->log.mod[P_LOG_BIOS] = P_LOG_TRACE;
-	cfg->log.mod[P_LOG_SCHED] = P_LOG_TRACE;
-	cfg->log.mod[P_LOG_GPU] = P_LOG_OFF;
-	cfg->log.mod[P_LOG_INTCTRL] = P_LOG_TRACE;
-
-	cfg->bios_trace.stdout_line = on_stdout_line;
-	cfg->bios_trace.deref_ptrs = true;
-
-	//cfg->disasm.tracing = true;
-
-	cfg->on_vblank = on_vblank;
-
-	p_init(&m_ctx);
+	emu_init();
 
 	if (!load_bios_file(argv[1]))
 		return EXIT_FAILURE;
@@ -218,51 +309,28 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	if (!SDL_Init(SDL_INIT_VIDEO)) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-			     "Couldn't initialize SDL: %s", SDL_GetError());
+	if (!gfx_init())
 		return EXIT_FAILURE;
-	}
 
-	if (!SDL_CreateWindowAndRenderer("psycho", 1920, 1080,
-					 SDL_WINDOW_RESIZABLE, &window,
-					 &renderer)) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-			     "Couldn't create window and renderer: %s",
-			     SDL_GetError());
-		return EXIT_FAILURE;
-	}
+	running = true;
 
-	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_XBGR1555,
-				    SDL_TEXTUREACCESS_STREAMING, VRAM_WIDTH,
-				    VRAM_HEIGHT);
-
-	SDL_RenderClear(renderer);
-	SDL_RenderTexture(renderer, texture, NULL, NULL);
-	SDL_RenderPresent(renderer);
-
-	for (;;) {
+	while (running) {
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
-			if (event.type == SDL_EVENT_QUIT)
-				goto end;
+			sdl_process_ev(&event);
 
-		Uint64 start_ticks = SDL_GetTicks();
+		const Uint64 start_ns = SDL_GetTicksNS();
 		p_run_until_ev(&m_ctx);
-		Uint64 end_ticks = SDL_GetTicks();
+		const Uint64 end_ns = SDL_GetTicksNS();
 
-		Uint64 ft = end_ticks - start_ticks;
+		const Uint64 diff = end_ns - start_ns;
 
-		if (ft < (1000 / 60))
-			SDL_Delay((1000 / 60) - ft);
+		if (diff < (1000000000 / 60))
+			SDL_DelayNS((1000000000 / 60) - diff);
+
+		fps_update();
 	}
 
-end:
-	SDL_DestroyTexture(texture);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-
-	SDL_Quit();
-
+	gfx_fini();
 	return EXIT_SUCCESS;
 }
